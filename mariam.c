6764530fd0883de0f89dea87678bbf31b7ecb77e
mariam adebisi <madebisi316@gmail.com>
2022-08-26 12:15:26 +0100

    my first c code

---
+++ mariam.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	/** declaring my variables
+	 * inc programing*/
+	int a;
+	int b;
+	int c;
+
+	//assigning the variables
+	a = 5;
+	b = 10;
+	c = 15;
+
+	printf("the sum of a and b is %d\n", a + b);
+	return (0);
+	
+}
